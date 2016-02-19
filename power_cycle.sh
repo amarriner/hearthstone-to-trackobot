31603bf49c07df18d2b63a86899cfccd3c62507b
Aaron Marriner <amarriner@amarriner.com>
2016-02-19 20:15:16 +0000

    Added sample inotify script

---
+++ power_cycle.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+HOME="/home/amarriner"
+BASE_DIR="${HOME}/Dropbox/Hearthstone/logs"
+PYTHON=${HOME}/.localpython-3.4.2/bin/python3.4
+HSREPLAY=${HOME}/python/HSReplay/python/convert.py
+HSPARSE=${HOME}/python/hearthstone-parse/parse.sh
+
+cd $BASE_DIR
+
+inotifywait -q --format "%w%f" -mr -e close . |
+   while read -r file; do
+      DIR=`echo "$file" | cut -f2 -d"/"`
+      FILE=`echo "$file" | cut -f3 -d"/"`
+      if [ "$FILE" = "Power.log" ]; then
+         if [ -f "$file" ]; then
+            TS=`date +%Y%m%d%H%M%S`
+            mv "$file" "$DIR/Power_${TS}.log"
+
+            $PYTHON $HSREPLAY "$DIR/Power_${TS}.log" > "$DIR/Power_${TS}.xml"
+            $HSPARSE "$DIR/Power_${TS}.xml"
+         fi
+      fi
+   done
